da231844d96b7eef52603f2993a77dc3615a8947
TTpoxoDuJl-MuMo-for-Polytech <31887973+TTpoxoDuJl-MuMo-for-Polytech@users.noreply.github.com>
2019-06-24 23:51:16 +0300

    переделка положения курсора, поддержка чисел и ключевых слоа

--- JSON_parser.lua
+++ JSON_parser.lua
@@ -1,25 +1,24 @@
 local M, file = {}, {}
 local char = ""
 
-local function next_char(leave_spaces)
+local function next_char(check_eof, leave_spaces)
     char = file:read(1)
-    if char == nil then
+    if check_eof and char == nil then
         error("unexpected end of file")
     elseif not leave_spaces and char == " " then
-        next_char()
+        next_char(check_eof)
     end
 end
 
 function parse_obj()
     local res
-    next_char()
     if char == "{" then
         res = parse_table()
     elseif char == "[" then
         res = parse_array()
     elseif char == "\"" then
         res = parse_string()
-    elseif string.match(char, "^%d$") then
+    elseif string.match(char, "^[%d%-]$") then
         res = parse_number()
     else
         res = parse_keyword()
@@ -30,55 +29,56 @@ end
 
 function parse_table()
     local res = {}
-    next_char()
---    if c ~= "{" then
---        error("invalid format")
---    end
 
+    if char ~= "{" then
+        error("table expected, but found" .. char)
+    end
+
+    next_char(true)
     while char ~= "}" do
         local key = parse_string()
-        next_char()
         if char ~= ":" then
             error("invalid format")
         end
 
+        next_char(true)
         local val = parse_obj()
 
-        next_char()
         if char ~= "}" and char ~= "," then
             error("unclosed table")
         elseif char == "," then
-            next_char()
+            next_char(true)
         end
 
         res[key] = val
     end
 
+    next_char(false)
     return res
 end
 
 function parse_array()
     local res = {}
-    next_char()
---    if c ~= "[" then
---        error("invalid format")
---    end
+    if char ~= "[" then
+        error("array expected, but found" .. char)
+    end
 
+    next_char(true)
     local i = 1
     while char ~= "]" do
         local val = parse_string()
 
-        next_char()
         if char ~= "]" and char ~= "," then
             error("unclosed array")
         elseif char == "," then
-            next_char()
+            next_char(true)
         end
 
         res[i] = val
         i = i + 1
     end
 
+    next_char(false)
     return res
 end
 
@@ -89,25 +89,50 @@ function parse_string()
     if char ~= "\"" then
         error("invalid format")
     end
-    next_char()
 
+    next_char(true)
     while char ~= "\"" do
         res = res .. char
-        next_char(true)
+        next_char(true, true)
     end
 
+    next_char(false)
     return res
 end
 
-function parse_number() end
+function parse_number()
+    local element = ""
+    while string.match(char, "^[%d%+%-%.Ee]$") do
+        element = element .. char
+        next_char(true)
+    end
 
-function parse_keyword() end
+    return tonumber(element)
+end
+
+function parse_keyword()
+    local element = ""
+    while string.match(char, "^[a-z]$") do
+        element = element .. char
+        next_char(true)
+    end
+
+    if element == "true" then
+        return true
+    elseif element == "false" then
+        return false
+    elseif element == "null" then
+        return nil
+    else
+        error("unexpected element")
+    end
+end
 
 function M.parse(file_name)
     file = io.open(file_name)
 
-    local res = parse_obj()
-    char = file:read(1)
+    next_char(true)
+    local res = parse_table()
     if char ~= nil then
         error("trash after main table found")
     end
--- tests/test.lua
+++ tests/test.lua
@@ -3,4 +3,6 @@ local result = JSON_parser.parse("tests/test.json")
 
 print(result["first"])
 print(result["second"][2])
+print(result["bool"])
+print(result["number"])
 print(#result["empty_array"])
